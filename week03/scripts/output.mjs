88f3993d99f4d5fbdc6fd580abebd3dab067fe68
jorgehslima <jorgehs.lima@hotmail.com>
2026-09-14 16:38:49 -0400

    upd section => to contain three output mjs

--- week03/scripts/output.mjs
+++ week03/scripts/output.mjs
@@ -1,19 +1,15 @@
 export function setTitle(course) {
-  document.querySelector("#courseName").textContent =
-    `${course.code} - ${course.name}`;
+  document.querySelector("#courseName").textContent = course.name;
+  document.querySelector("#courseCode").textContent = course.code;
 }
 
 export function renderSections(sections) {
   const html = sections.map(
-    (section) => `
-      <tr>
-        <td>${section.sectionNum}</td>
-        <td>${section.roomNum}</td>
-        <td>${section.enrolled}</td>
-        <td>${section.days}</td>
-        <td>${section.instructor}</td>
-      </tr>
-    `
+    (section) => `<tr>
+      <td>${section.sectionNumber}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.instructor}</td>
+    </tr>`
   );
 
   document.querySelector("#sections").innerHTML = html.join("");
