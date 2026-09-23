7559656d1d1fdfda8318bce70729671dd587585a
jorgehslima <jorgehs.lima@hotmail.com>
2026-09-23 12:36:44 -0400

    upd scripts js

--- week04/startHere/js/scripts.js
+++ week04/startHere/js/scripts.js
@@ -4,7 +4,6 @@
 const myInfor = new URLSearchParams(window.location.search);
 //console.log(myInfo);
 
-
 document.querySelector('results').innerHTML = `
 <p>Appointment for ${myInfo.get('first')} ${myInfo.get('last')}</p>
 <p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
